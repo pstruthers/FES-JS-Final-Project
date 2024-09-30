fc83552ce0de2b87b16fe9e3194517cc6488ff2e
Paige <paige.struthers@gmail.com>
2024-09-30 18:49:35 -0500

    game list display UI

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// API Key: 58ee01e52ce14968a6c26b86c06b3f2b
 const gameList = document.querySelector(".game-list");
 
 async function main() {
@@ -13,24 +12,43 @@ async function main() {
 main();
 
 function gameHTML(game) {
-    const gamePlatformsShort = {
-        "PlayStation 5" : "PS5",
-        "PlayStation 4" : "PS4",
-        "PlayStation 3" : "PS3",
-        "PlayStation 2" : "PS2",
-        "PlayStation 1" : "PS1",
-        "PlayStation" : "PS1"
-    };
+  const gamePlatformsShort = {
+    "PlayStation 5": `<i class="fa-brands fa-playstation"></i>`,
+    "PlayStation 4": `<i class="fa-brands fa-playstation"></i>`,
+    "PlayStation 3": `<i class="fa-brands fa-playstation"></i>`,
+    "PlayStation 2": `<i class="fa-brands fa-playstation"></i>`,
+    "PlayStation 1": `<i class="fa-brands fa-playstation"></i>`,
+    "PlayStation": `<i class="fa-brands fa-playstation"></i>`,
+    "Xbox" : `<i class="fa-brands fa-xbox"></i>`,
+    "Xbox 360" : `<i class="fa-brands fa-xbox"></i>`,
+    "Xbox One" : `<i class="fa-brands fa-xbox"></i>`,
+    "Xbox Series S/X" : `<i class="fa-brands fa-xbox"></i>`,
+    "Linux" : `<i class="fa-brands fa-linux"></i>`,
+    "PC" : `<i class="fa-brands fa-windows"></i>`,
+    "macOS": `<i class="fa-solid fa-desktop"></i>`,
+    "iOS" : `<i class="fa-brands fa-apple"></i>`,
+    "Android" : `<i class="fa-brands fa-android"></i>`,
+    "Nintendo Switch" : `<i class="fa-solid fa-n"></i>`
+  };
 
-  const gamePlatforms = game.platforms.map(platform => gamePlatformsShort[platform.platform.name] || platform.platform.name).sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase())).join(", ");
+  const uniqueLogos = new Set();
 
-  const gameGenres = game.genres.map(genre => genre.name).join(", ");
+  game.platforms.forEach(platform => {
+    const logo = gamePlatformsShort[platform.platform.name];
+    if (logo) {
+      uniqueLogos.add(logo);
+    }
+  });
+
+  const sortedUniqueLogos = Array.from(uniqueLogos).sort().join("");
+
+  const gameGenres = game.genres.map((genre) => genre.name).join(", ");
 
   const releaseDate = new Date(game.released);
   const formattedReleaseDate = releaseDate.toLocaleDateString("en-US", {
     year: "numeric",
     month: "short",
-    day: "numeric"
+    day: "numeric",
   });
 
   let scoreColor = "";
@@ -42,15 +60,21 @@ function gameHTML(game) {
     scoreColor = "score--red";
   }
 
-    return `<div class="game-card">
+  return `<div class="game-card">
             <div class="game-card__container">
-            <img src="${game.background_image}" alt="">
-            <h5>${game.name}</h5>
-            <h6>${formattedReleaseDate}</h6>
-            <h6>${gamePlatforms}</h6>
-            <h6>${gameGenres}</h6>
-            <h6>Average Playtime (hours): ${game.playtime}</h6>
-            <h6><span class="${scoreColor}">${game.metacritic}</span></h6>
-            </div>
-        </div>`;
+              <figure class="game__img--wrapper">
+                <img src="${game.background_image}" class="game__img" alt="">
+              </figure>
+              <div class="game__info">
+                <h5 class="game__title">${game.name}</h5>
+                <h6 class="game__release-date">${formattedReleaseDate}</h6>
+                <h6 class="game__platforms">${sortedUniqueLogos}</h6>
+                <div class="genres">
+                  <h6 class="genres__label">Genres:</h6>
+                  <h6 class="game__genres">${gameGenres}</h6>
+                </div>
+                <h6 class="game__score"><span class="${scoreColor}">${game.metacritic}</span></h6>
+                </div>
+              </div>
+            </div>`;
 }
